beac229a2bed3c0d4b20b80f372a13bb44586b19
Swell <2653328255@qq.com>
2019-10-30 10:01:43 +0800

    add

--- driver/pages/order/order.vue
+++ driver/pages/order/order.vue
@@ -50,43 +50,45 @@
 			</swiper-item>
 			<swiper-item>
 				<scroll-view class="scroll-container">
-					<view class="ui-order-list-item ui-daizhifu">
-						<view class="dui-gap"></view>
-						<view class="dui-gap"></view>
-						<view class="ui-order-list-item-top"><text class="ui-orderno-color">20191010101012341456</text></view>
-						<view class="ui-order-list-item-top"><text class="ui-cartype-color">09月20日 10:22</text></view>
-						<view class="ui-order-list-item-top"><text>货物类型: <text class="ui-cartype-color">整车</text></text> </view>
-						<view class="ui-divide-line"></view>
-						<view class="ui-order-timeline-container">
-							<view class="ui-order-timeline uni-timeline">
-								<view class="uni-timeline-item uni-timeline-first-item">
-									<view class="uni-timeline-item-divider"></view>
-									<view class="uni-timeline-item-content">
-										<text class="ui-address">衡水人民政府</text>
+					<navigator url="/pages/order/photoupload">
+						<view class="ui-order-list-item ui-daizhifu">
+							<view class="dui-gap"></view>
+							<view class="dui-gap"></view>
+							<view class="ui-order-list-item-top"><text class="ui-orderno-color">20191010101012341456</text></view>
+							<view class="ui-order-list-item-top"><text class="ui-cartype-color">09月20日 10:22</text></view>
+							<view class="ui-order-list-item-top"><text>货物类型: <text class="ui-cartype-color">整车</text></text> </view>
+							<view class="ui-divide-line"></view>
+							<view class="ui-order-timeline-container">
+								<view class="ui-order-timeline uni-timeline">
+									<view class="uni-timeline-item uni-timeline-first-item">
+										<view class="uni-timeline-item-divider"></view>
+										<view class="uni-timeline-item-content">
+											<text class="ui-address">衡水人民政府</text>
+										</view>
+									</view>
+									<view class="uni-timeline-item uni-timeline-last-item">
+										<view class="uni-timeline-item-divider"></view>
+										<view class="uni-timeline-item-content">
+											<text class="ui-address">怡然城</text>
+										</view>
 									</view>
 								</view>
-								<view class="uni-timeline-item uni-timeline-last-item">
-									<view class="uni-timeline-item-divider"></view>
-									<view class="uni-timeline-item-content">
-										<text class="ui-address">怡然城</text>
-									</view>
-								</view>
+								<view class="ui-order-item-more"><text class="iconfont icon-xiayiyeqianjinchakangengduo"></text></view>
 							</view>
-							<view class="ui-order-item-more"><text class="iconfont icon-xiayiyeqianjinchakangengduo"></text></view>
+							<view class="ui-divide-line"></view>
+							<view class="dui-gap"></view>
+							<view class="ui-od-price-list">
+								<view class="ui-od-price-type">需要加急?</view>
+								<view>否</view>
+							</view>
 						</view>
-						<view class="ui-divide-line"></view>
-						<view class="dui-gap"></view>
-						<view class="ui-od-price-list">
-							<view class="ui-od-price-type">需要加急?</view>
-							<view>否</view>
+						<view class="ui-orderdetail-cont">
+							<view class="ui-od-price-list">
+								<view class="ui-od-price-type">客户已支付</view>
+								<view class="ui-od-price-pay">￥23.09</view>
+							</view>
 						</view>
-					</view>
-					<view class="ui-orderdetail-cont">
-						<view class="ui-od-price-list">
-							<view class="ui-od-price-type">客户已支付</view>
-							<view class="ui-od-price-pay">￥23.09</view>
-						</view>
-					</view>
+					</navigator>
 				</scroll-view>
 			</swiper-item>
 			<swiper-item>
