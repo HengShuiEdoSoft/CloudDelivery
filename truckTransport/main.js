d1aa0535e92238a644876acd7926c2144203ec3e
francesLy <1096074393@qq.com>
2019-11-18 20:19:51 +0800

    map

--- truckTransport/main.js
+++ truckTransport/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App'
-
 import store from './store';
 import drmking from './common/drmking.js';
 import uniFly from 'unifly';
@@ -65,6 +64,23 @@ uniFly.responseInterceptors.error = function(response) {
 			params: params
 		});
 	}
+}
+Date.prototype.Format = function(fmt) { //author: meizz
+	var o = {
+		"M+": this.getMonth() + 1, //月份
+		"d+": this.getDate(), //日
+		"h+": this.getHours(), //小时
+		"m+": this.getMinutes(), //分
+		"s+": this.getSeconds(), //秒
+		"q+": Math.floor((this.getMonth() + 3) / 3), //季度
+		"S": this.getMilliseconds() //毫秒
+	};
+	if (/(y+)/.test(fmt))
+		fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+	for (var k in o)
+		if (new RegExp("(" + k + ")").test(fmt))
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
 }
 Vue.config.productionTip = false
 Vue.prototype.$store = store
