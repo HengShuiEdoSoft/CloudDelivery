dd8f53ed8addbe40a3cebe97929237de4d1be956
c517718004 <517718004@qq.com>
2019-11-29 21:39:05 +0800

    修复完成充值页面

--- truckTransport/pages/mymoney/myrecharge.vue
+++ truckTransport/pages/mymoney/myrecharge.vue
@@ -3,40 +3,30 @@
 		<view class="ui-divide-line"></view>
 		<scroll-view scroll-y="true">
 			<view class="dui-recharge-list">
-				<view class="dui-recharge-item" :class="{active:key===cur}" v-for="(item,key) in list" :key="key" @tap="chargrchange(key)">
+				<view class="dui-recharge-item" :class="{ active: index === cur }" v-for="(item, index) in list" :key="item.pay_cate_id" @tap="chargrchange(index)">
 					<view>
-					<view class="dui-recharge-no">
-						{{item.pay_money}}元
-					</view>
-					<view class="dui-recharge-describe">
-						送{{item.coupon_price_total}}优惠券
-					</view>
+						<view class="dui-recharge-no">{{ item.pay_money }}元</view>
+						<view class="dui-recharge-describe">送{{ item.coupon_price_total }}优惠券</view>
 					</view>
 				</view>
 			</view>
 			<view class="dui-recharge-firstcharge">
-				首值礼包 <text class="iconfont icon-liwu"></text>
+				充值礼包
+				<text class="iconfont icon-liwu"></text>
 			</view>
-			<view class="dui-recharge-firstcharge-body">
-				<view>赠送20元优惠券</view>
-				<view>券包含 10元立减券×2张</view>
+			<view class="dui-recharge-firstcharge-body" v-if="list.length > 0">
+				<view v-for="(item, index) in list[cur].coupon_list">赠送 {{ item.coupon_title }} x {{ item.coupon_num }}张</view>
 			</view>
 			<view class="dui-recharge-paymode">
-				<view class="dui-recharge-paymode-title">
-					支付方式
-				</view>
+				<view class="dui-recharge-paymode-title">支付方式</view>
 				<view class="dui-paymode-list">
 					<radio-group @change="radioChange">
 						<label class="dui-paymode-list-item" v-for="(item, index) in items" :key="item.value">
 							<view class="dui-paymode-title">
 								<view :class="item.icon"></view>
-								<view class="dui-paymode-name">
-									{{item.name}}
-								</view>
+								<view class="dui-paymode-name">{{ item.name }}</view>
 							</view>
-							<view>
-								<radio :value="item.value" :checked="index === current" color="#FF5723" />
-							</view>
+							<view><radio :value="item.value" :checked="index === current" color="#FF5723" /></view>
 						</label>
 					</radio-group>
 				</view>
@@ -46,134 +36,129 @@
 			<view @tap="gotopay">
 				<button class="primary" type="primary">
 					立即充值
-					<view class="dui-fixed-bottom-tip">
-						(实际到账{{pay_money}}元)
-					</view>
+					<view class="dui-fixed-bottom-tip">(实际到账{{ pay_money }}元)</view>
 				</button>
 			</view>
 		</view>
 	</view>
 </template>
 <script>
-	import { mapState } from 'vuex';
-	export default {
-		data() {
-			return {
-				type: "",
-				cur:'',
-				current: 0,
-				items: [{
-					name: "支付宝",
-					value: "alipay",
-					icon: "iconfont icon-z-alipay"
-				}, {
-					name: "微信",
-					value: "wxpay",
-					icon: "iconfont icon-weixin"
-				}],
-				list:{},
-				pay_money:0,
-				pay_type:0,
-				pay_cate_id:0,
-				pay_platform:'',
-				provider:'alipay'
-				
+import { mapState } from 'vuex';
+export default {
+	data() {
+		return {
+			type: '',
+			cur: 0,
+			current: 0,
+			items: [
+				{
+					name: '支付宝',
+					value: 'alipay',
+					icon: 'iconfont icon-z-alipay'
+				},
+				{
+					name: '微信',
+					value: 'wxpay',
+					icon: 'iconfont icon-weixin'
+				}
+			],
+			list: [],
+			pay_money: 0,
+			pay_type: 0,
+			pay_cate_id: 0,
+			pay_platform: '',
+			provider: 'alipay'
+		};
+	},
+	onLoad() {
+		this.getdata();
+		let that = this;
+		// #ifdef MP-WEIXIN
+		that.pay_platform = 'miniapp';
+		// #endif
+
+		// #ifdef APP-PLUS
+		that.pay_platform = 'app';
+		// #endif
+
+		// #ifdef H5
+		that.pay_platform = 'web';
+		// #endif
+	},
+	computed: mapState(['user']),
+	methods: {
+		radioChange: function(evt) {
+			for (let i = 0; i < this.items.length; i++) {
+				if (this.items[i].value === evt.target.value) {
+					this.current = i;
+					this.provider = this.items[i].value;
+					break;
+				}
 			}
 		},
-		onLoad(){
-			this.getdata();
-			let that = this;
-			// #ifdef MP-WEIXIN
-			that.pay_platform = 'miniapp';
-			// #endif
-			
-			// #ifdef APP-PLUS
-			that.pay_platform = 'app';
-			// #endif
-			
-			// #ifdef H5
-			that.pay_platform = 'web';
-			// #endif
-			
+		chargrchange: function(index) {
+			this.cur = index;
+			this.pay_money = this.list[index].pay_money;
+			this.pay_cate_id = this.list[index].pay_cate_id;
 		},
-		computed: mapState(['user']),
-		methods: {
-			radioChange: function(evt) {
-				for (let i = 0; i < this.items.length; i++) {
-					if (this.items[i].value === evt.target.value) {
-						this.current = i;
-						this.provider=this.items[i].value;
-						break;
-					}
-				}
-			},
-			chargrchange:function(i){
-				this.cur = i;
-				this.pay_money=this.list.i.pay_money;
-				this.pay_id=this.list.i.pay_id;
-			},
-			getdata:function(){
-				let that=this
-				this.$uniFly
-					.get({
-						url: '/api/pay_cate/getpaycatelist',
-						params:{}
-					})
-					.then(function(res) {
-						if (res.code === 0 ) {
-							that.list=res.data;						
-						} else {
-							uni.showModal({
-								content: res.msg,
-								showCancel: false
-							});
+		getdata: function() {
+			let that = this;
+			this.$uniFly
+				.get({
+					url: '/api/pay_cate/getpaycatelist',
+					params: {}
+				})
+				.then(function(res) {
+					if (res.code == 0) {
+						let list = [];
+						for (let i in res.data) {
+							list.push(res.data[i]);
 						}
-					})
-					.catch(function(error) {
+						that.list = list;
+						if (list.length > 0) {
+							that.pay_cate_id = list[0].pay_cate_id;
+							that.pay_money = list[0].pay_money;
+						}
+					} else {
 						uni.showModal({
-							content: error,
+							content: res.msg,
 							showCancel: false
 						});
+					}
+				})
+				.catch(function(error) {
+					uni.showModal({
+						content: error,
+						showCancel: false
 					});
-			},
-			gotopay:function(){
-				this.addpaylog();
-			},
-			addpaylog() {
-				let that = this;
-				if (that.pay_id > 0 && that.pay_money > 0) {
-					uni.showLoading({
-						title: '正在创建订单...'
-					});
-					that.$uniFly
-						.post({
-							url: '/api/pay_log/addpaylog',
-							params: {
-								pay_id: that.pay_id,
-								pay_type: that.pay_type,
-								pay_money: that.pay_money
-							}
-						})
-						.then(res => {
-							uni.hideLoading();
-							if (res.code == 0) {
-								that.pay_log_id = res.data;
-								that.pay();
-							} else {
-								uni.showModal({
-									title: '支付失败，重新尝试？',
-									content: res.msg,
-									success: function(res) {
-										if (res.confirm) {
-											that.addpaylog();
-										} else if (res.cancel) {
-											return;
-										}
-									}
-								});
-							}
-						})
-						.catch(err => {
+				});
+		},
+		gotopay: function() {
+			this.addpaylog();
+		},
+		addpaylog() {
+			let that = this;
+			if (that.pay_cate_id > 0 && that.pay_money > 0) {
+				uni.showLoading({
+					title: '正在创建订单...'
+				});
+				let paylog_data = {
+					order_id: that.pay_cate_id,
+					pay_type: that.pay_type,
+					pay_money: that.pay_money
+				};
+				console.log(paylog_data);
+				that.$uniFly
+					.post({
+						url: '/api/pay_log/addpaylog',
+						params: paylog_data
+					})
+					.then(res => {
+						uni.hideLoading();
+						if (res.code == 0) {
+							that.pay_log_id = res.data;
+							that.pay();
+						} else {
 							uni.showModal({
 								title: '支付失败，重新尝试？',
 								content: res.msg,
@@ -185,129 +170,128 @@
 									}
 								}
 							});
+						}
+					})
+					.catch(err => {
+						uni.showModal({
+							title: '支付失败，重新尝试？',
+							content: res.msg,
+							success: function(res) {
+								if (res.confirm) {
+									that.addpaylog();
+								} else if (res.cancel) {
+									return;
+								}
+							}
 						});
-				} else {
-					uni.showToast({
-						icon: 'none',
-						title: '订单不存在!'
 					});
-					return;
-				}
-			},
-			pay() {
-				let that = this;
-				if (that.pay_log_id > 0) {
-					uni.showLoading({
-						title: '支付中...'
-					});
-					console.log({
-								pay_log_id: that.pay_log_id,
-								pay_platform: that.pay_platform,
+			} else {
+				uni.showToast({
+					icon: 'none',
+					title: '订单不存在!'
+				});
+				return;
+			}
+		},
+		pay() {
+			let that = this;
+			if (that.pay_log_id > 0) {
+				uni.showLoading({
+					title: '支付中...'
+				});
+				console.log({
+					pay_log_id: that.pay_log_id,
+					pay_platform: that.pay_platform,
+					provider: that.provider,
+					openid: that.user.minwxapp_id
+				});
+				that.$uniFly
+					.post({
+						url: '/api/pay_log/pay',
+						params: {
+							pay_log_id: that.pay_log_id,
+							pay_platform: that.pay_platform,
+							provider: that.provider,
+							openid: that.user.minwxapp_id
+						}
+					})
+					.then(res => {
+						uni.hideLoading();
+						if (res.code == 0) {
+							// 微信或支付宝支付,调用uni.requestPayment,拉起第三方支付
+							// 微信小程序支付
+							// #ifdef MP-WEIXIN
+							uni.requestPayment({
 								provider: that.provider,
-								openid: that.user.minwxapp_id
+								orderInfo: res.data.data, //微信、支付宝订单数据
+								timeStamp: res.data.data.timeStamp,
+								nonceStr: res.data.data.nonceStr,
+								package: res.data.data.package,
+								signType: res.data.data.signType,
+								paySign: res.data.data.paySign,
+								success: function(res) {
+									console.log('success:' + JSON.stringify(res));
+									uni.showToast({
+										icon: 'none',
+										title: '支付完成！'
+									});
+									setTimeout(function() {
+										uni.navigateTo({
+											url: '/pages/index/index'
+										});
+									}, 1500);
+								},
+								fail: function(err) {
+									console.log('fail:' + JSON.stringify(err));
+									uni.showModal({
+										title: '支付失败，重新尝试？',
+										content: res.msg,
+										success: function(res) {
+											if (res.confirm) {
+												that.pay();
+											} else if (res.cancel) {
+												return;
+											}
+										}
+									});
+								}
 							});
-					that.$uniFly
-						.post({
-							url: '/api/pay_log/pay',
-							params: {
-								pay_log_id: that.pay_log_id,
-								pay_platform: that.pay_platform,
+							// #endif
+							// app支付
+							// #ifdef APP-PLUS
+							console.log(res.data);
+							uni.requestPayment({
 								provider: that.provider,
-								openid: that.user.minwxapp_id
-							}
-						})
-						.then(res => {
-							uni.hideLoading();
-							if (res.code == 0) {
-									// 微信或支付宝支付,调用uni.requestPayment,拉起第三方支付
-									// 微信小程序支付
-									// #ifdef MP-WEIXIN
-									uni.requestPayment({
-										provider: that.provider,
-										orderInfo: res.data.data, //微信、支付宝订单数据
-										timeStamp: res.data.data.timeStamp,
-										nonceStr: res.data.data.nonceStr,
-										package: res.data.data.package,
-										signType: res.data.data.signType,
-										paySign: res.data.data.paySign,
+								orderInfo: res.data, //微信、支付宝订单数据
+								success: function(res) {
+									console.log('success:' + JSON.stringify(res));
+									uni.showToast({
+										icon: 'none',
+										title: '支付完成！'
+									});
+									setTimeout(function() {
+										uni.navigateTo({
+											url: '/pages/index/index'
+										});
+									}, 1500);
+								},
+								fail: function(err) {
+									console.log('fail:' + JSON.stringify(err));
+									uni.showModal({
+										title: '支付失败，重新尝试？',
+										content: res.msg,
 										success: function(res) {
-											console.log('success:' + JSON.stringify(res));
-											uni.showToast({
-												icon: 'none',
-												title: '支付完成！'
-											});
-											setTimeout(function() {
-												uni.navigateTo({
-													url: '/pages/index/index'
-												});
-											}, 1500);
-										},
-										fail: function(err) {
-											console.log('fail:' + JSON.stringify(err));
-											uni.showModal({
-												title: '支付失败，重新尝试？',
-												content: res.msg,
-												success: function(res) {
-													if (res.confirm) {
-														that.pay();
-													} else if (res.cancel) {
-														return;
-													}
-												}
-											});
+											if (res.confirm) {
+												that.pay();
+											} else if (res.cancel) {
+												return;
+											}
 										}
 									});
-									// #endif
-									// app支付
-									// #ifdef APP-PLUS
-									console.log(res.data);
-									uni.requestPayment({
-										provider: that.provider,
-										orderInfo: res.data, //微信、支付宝订单数据
-										success: function(res) {
-											console.log('success:' + JSON.stringify(res));
-											uni.showToast({
-												icon: 'none',
-												title: '支付完成！'
-											});
-											setTimeout(function() {
-												uni.navigateTo({
-													url: '/pages/index/index'
-												});
-											}, 1500);
-										},
-										fail: function(err) {
-											console.log('fail:' + JSON.stringify(err));
-											uni.showModal({
-												title: '支付失败，重新尝试？',
-												content: res.msg,
-												success: function(res) {
-													if (res.confirm) {
-														that.pay();
-													} else if (res.cancel) {
-														return;
-													}
-												}
-											});
-										}
-									});
-									// #endif
-							} else {
-								uni.showModal({
-									title: '订单支付失败，重新尝试？',
-									content: res.msg,
-									success: function(res) {
-										if (res.confirm) {
-											that.pay();
-										} else if (res.cancel) {
-											that.$refs['pay'].close();
-											return;
-										}
-									}
-								});
-							}
-						})
-						.catch(err => {
+								}
+							});
+							// #endif
+						} else {
 							uni.showModal({
 								title: '订单支付失败，重新尝试？',
 								content: res.msg,
@@ -315,149 +299,161 @@
 									if (res.confirm) {
 										that.pay();
 									} else if (res.cancel) {
-										that.$refs['pay'].close();
 										return;
 									}
 								}
 							});
+						}
+					})
+					.catch(err => {
+						uni.showModal({
+							title: '订单支付失败，重新尝试？',
+							content: res.msg,
+							success: function(res) {
+								if (res.confirm) {
+									that.pay();
+								} else if (res.cancel) {
+									return;
+								}
+							}
 						});
-				} else {
-					uni.showToast({
-						icon: 'none',
-						title: '订单不存在!'
 					});
-					that.$refs['pay'].close();
-					return;
-				}
+			} else {
+				uni.showToast({
+					icon: 'none',
+					title: '订单不存在!'
+				});
+				return;
 			}
 		}
 	}
+};
 </script>
 
 <style>
-	.content {
-		background-color: #fff;
-	}
+.content {
+	background-color: #fff;
+}
 
-	scroll-view {
-		margin-bottom: 140upx;
-	}
+scroll-view {
+	margin-bottom: 140upx;
+}
 
-	.dui-recharge-list {
-		display: flex;
-		flex-wrap: wrap;
-		padding: 30upx 15upx;
-	}
+.dui-recharge-list {
+	display: flex;
+	flex-wrap: wrap;
+	padding: 30upx 15upx;
+}
 
-	.dui-recharge-item {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		margin-top: 10px;
-		padding:0 10upx;
-		width: 240upx;
-		box-sizing: border-box;
-		text-align: center;	
-	}
-	.dui-recharge-item>view{
-		padding:40upx 0;
-		width:100%;
-		box-sizing: border-box;
-		background-color: #f0f4f7;
-	}
-	.dui-recharge-item.active>view{
-		background-color: #fdf4ef;
-		border: 2upx solid #FF5723;
-	}
+.dui-recharge-item {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin-top: 10px;
+	padding: 0 10upx;
+	width: 240upx;
+	box-sizing: border-box;
+	text-align: center;
+}
+.dui-recharge-item > view {
+	padding: 40upx 0;
+	width: 100%;
+	box-sizing: border-box;
+	background-color: #f0f4f7;
+}
+.dui-recharge-item.active > view {
+	background-color: #fdf4ef;
+	border: 2upx solid #ff5723;
+}
 
-	.dui-recharge-no {
-		font-size: 32upx;
-	}
+.dui-recharge-no {
+	font-size: 32upx;
+}
 
-	.dui-recharge-describe {
-		font-size: 24upx;
-		color: #5f6062;
-	}
+.dui-recharge-describe {
+	font-size: 24upx;
+	color: #5f6062;
+}
 
-	.dui-recharge-firstcharge {
-		margin-top: 15upx;
-		width: 200upx;
-		height: 50upx;
-		line-height: 50upx;
-		background-color: #FF5723;
-		border-radius: 0 25upx 25upx 0;
-		text-align: center;
-		color: #fff;
-		font-size: 28upx;
-	}
+.dui-recharge-firstcharge {
+	margin-top: 15upx;
+	width: 200upx;
+	height: 50upx;
+	line-height: 50upx;
+	background-color: #ff5723;
+	border-radius: 0 25upx 25upx 0;
+	text-align: center;
+	color: #fff;
+	font-size: 28upx;
+}
 
-	.dui-recharge-firstcharge .icon-liwu {
-		margin-left: 10upx;
-	}
+.dui-recharge-firstcharge .icon-liwu {
+	margin-left: 10upx;
+}
 
-	.dui-recharge-firstcharge-body {
-		margin-left: 40upx;
-		padding: 24upx 0;
-		border-bottom: 1upx solid #e2e2e2;
-	}
+.dui-recharge-firstcharge-body {
+	margin-left: 40upx;
+	padding: 24upx 0;
+	border-bottom: 1upx solid #e2e2e2;
+}
 
-	.dui-recharge-firstcharge-body view:first-child {
-		font-size: 28upx;
-		color: #FF5723;
-	}
+.dui-recharge-firstcharge-body view:first-child {
+	font-size: 28upx;
+	color: #ff5723;
+}
 
-	.dui-recharge-firstcharge-body view:nth-child(2) {
-		width: 80%;
-		font-size: 24upx;
-		color: #919191;
-	}
+.dui-recharge-firstcharge-body view:nth-child(2) {
+	width: 80%;
+	font-size: 24upx;
+	color: #919191;
+}
 
-	.dui-recharge-paymode {
-		margin-left: 40upx;
-	}
+.dui-recharge-paymode {
+	margin-left: 40upx;
+}
 
-	.dui-recharge-paymode-title {
-		font-size: 30upx;
-		padding-top: 20upx;
-	}
+.dui-recharge-paymode-title {
+	font-size: 30upx;
+	padding-top: 20upx;
+}
 
-	.dui-paymode-list {
-		font-size: 14px;
-		line-height: 84upx;
-	}
+.dui-paymode-list {
+	font-size: 14px;
+	line-height: 84upx;
+}
 
-	.dui-paymode-list-item {
-		display: flex;
-		align-items: center;
-		margin: 20upx 40upx 20upx 10upx;
-	}
+.dui-paymode-list-item {
+	display: flex;
+	align-items: center;
+	margin: 20upx 40upx 20upx 10upx;
+}
 
-	.dui-paymode-title {
-		flex: 1;
-	}
+.dui-paymode-title {
+	flex: 1;
+}
 
-	.dui-paymode-name {
-		display: inline-block;
-	}
+.dui-paymode-name {
+	display: inline-block;
+}
 
-	.dui-paymode-list .iconfont {
-		display: inline-block;
-		vertical-align: top;
-		font-size: 32px;
-		padding-right: 14px;
-	}
+.dui-paymode-list .iconfont {
+	display: inline-block;
+	vertical-align: top;
+	font-size: 32px;
+	padding-right: 14px;
+}
 
-	.icon-z-alipay {
-		color: #45B1FF;
-	}
+.icon-z-alipay {
+	color: #45b1ff;
+}
 
-	.icon-weixin {
-		color: #59F04C;
-	}
+.icon-weixin {
+	color: #59f04c;
+}
 
-	.dui-fixed-bottom-tip {
-		font-size: 24upx;
-		margin-top: -30upx;
-	}
+.dui-fixed-bottom-tip {
+	font-size: 24upx;
+	margin-top: -30upx;
+}
 </style>
