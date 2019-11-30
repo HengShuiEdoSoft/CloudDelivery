2e86a5a20003ba60d32b44805eee1a5f9b1e64e0
c517718004 <517718004@qq.com>
2019-11-30 19:35:28 +0800

    修改webscoket

--- truckTransport/main.js
+++ truckTransport/main.js
@@ -10,7 +10,7 @@ uniFly.baseUrl = 'https://www.lh5256.com';
 uniFly.timeOut = 20000
 // 错误自动尝试次数
 uniFly['retry_number'] = 3;
-//uniFly.headers={'Access-Control-Allow-Origin':'*'};
+
 //自定义请求拦截
 uniFly.requestInterceptors.success = function(request) {
 	let userinfo = drmking.cacheData('USER');
