58e9acd3f1b69d894506a66d8efb91503c03fab0
c517718004 <517718004@qq.com>
2019-11-30 19:46:07 +0800

    修改距离技术，改为向上取整

--- truckTransport/store/index.js
+++ truckTransport/store/index.js
@@ -168,7 +168,7 @@ const store = new Vuex.Store({
 		// 切换线路,更新价格
 		set_order_trip: function(state, trip_distance) {
 			Vue.set(state.order, 'trip', trip_distance.trip);
-			trip_distance.distance = parseInt(parseInt(trip_distance.distance) / 1000);
+			trip_distance.distance = Math.ceil(parseInt(trip_distance.distance) / 1000);
 			Vue.set(state.order, 'distance', trip_distance.distance);
 			let distance_price = drmking.distancePrice(state.order.car_base_price_json, trip_distance.distance);
 			Vue.set(state.order, 'distance_price', distance_price);
