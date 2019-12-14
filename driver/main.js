ec53ea512a9e9e8ea0ee668972215d284348b4c0
c517718004 <517718004@qq.com>
2019-12-14 23:41:49 +0800

    修正scoket

--- driver/main.js
+++ driver/main.js
@@ -117,7 +117,6 @@ function createWs() {
 			},
 			fail: function(err) {
 				ws_lock_reconnect = false;
-				uni.closeSocket();
 				console.log('webscoket 创建失败!' + err);
 				reconnectWs(url);
 			}
@@ -145,10 +144,10 @@ function reconnectWs(url) {
 let heartCheck = {
 	timeout: 30000, //30秒钟发一次心跳
 	timeoutObj: null,
-	serverTimeoutObj: null,
+	// serverTimeoutObj: null,
 	reset: function() {
 		clearTimeout(this.timeoutObj);
-		clearTimeout(this.serverTimeoutObj);
+		// clearTimeout(this.serverTimeoutObj);
 		return this;
 	},
 	start: function() {
@@ -157,13 +156,19 @@ let heartCheck = {
 			//这里发送一个心跳，后端收到后，返回一个心跳消息，
 			//onmessage拿到返回的心跳就说明连接正常
 			uni.sendSocketMessage({
-				data: 'ping'
+				data: 'ping',
+				complete: function(res) {
+					console.log(res);
+				}
 			});
-			self.serverTimeoutObj = setTimeout(function() {
-				//如果超过一定时间还没重置，说明后端主动断开了
-				//如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
-				uni.closeSocket();
+			setTimeout(function() {
+				self.start();
 			}, self.timeout);
+			// self.serverTimeoutObj = setTimeout(function() {
+			// 	//如果超过一定时间还没重置，说明后端主动断开了
+			// 	//如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
+			// 	uni.closeSocket();
+			// }, self.timeout);
 		}, this.timeout)
 	}
 }
@@ -193,13 +198,17 @@ let sendDriveLocation = {
 						}
 					});
 					uni.sendSocketMessage({
-						data: msg
+						data: msg,
+						complete: function(res) {
+							console.log(res);
+						}
 					});
 					setTimeout(function() {
 						self.start(user_id);
 					}, self.timeout);
 				})
 				.catch(err => {
+					console.log(err);
 					setTimeout(function() {
 						self.start(user_id);
 					}, self.timeout);
@@ -211,6 +220,7 @@ let sendDriveLocation = {
 function initEventHandle(url, user_id) {
 	// 监听WebSocket连接打开事件
 	uni.onSocketOpen(function(res) {
+		console.log('ws连接成功!' + new Date().toUTCString());
 		amap.getRegeo().then(res => {
 				let info = res[0];
 				let msg = JSON.stringify({
@@ -223,22 +233,22 @@ function initEventHandle(url, user_id) {
 					}
 				});
 				uni.sendSocketMessage({
-					data: msg
+					data: msg,
+					complete: function(res) {
+						console.log(res);
+					}
 				});
 			})
-			.catch(err => {
-
-			});
+			.catch(err => {});
 		//心跳检测重置
 		heartCheck.reset().start();
 		// 发送司机位置
 		sendDriveLocation.reset().start(user_id);
-		console.log('ws连接成功!' + new Date().toUTCString());
 	});
 	// 监听WebSocket错误
 	uni.onSocketError(function(res) {
-		reconnectWs(url);
 		console.log(res, 'ws连接错误!' + new Date().toUTCString());
+		reconnectWs(url);
 	});
 	// 监听WebSocket关闭
 	uni.onSocketClose(function(res) {
